e93a751434675faf069389946861c72294436658
Jeffrey <agiwar791005@gmail.com>
2025-12-03 20:48:43 +0800

    refactor: restructure to minimal MVP structure

--- src/reade/core/enums/db_types.py
+++ src/reade/core/enums/db_types.py
@@ -1,11 +1,1 @@
 """Database type enumeration."""
-
-from enum import Enum
-
-
-class DBTypes(str, Enum):
-    """Supported database types."""
-
-    POSTGRESQL = "postgresql"
-    MYSQL = "mysql"
-    SQLITE = "sqlite"
